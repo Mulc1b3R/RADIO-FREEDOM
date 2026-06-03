62df88d21dc38e8ca45ac8839da0efc28274f441
psico-mulciber <158625834+Mulc1b3R@users.noreply.github.com>
2026-06-03 14:23:42 +0100

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- main.js
+++ main.js
@@ -420,7 +420,10 @@ const crawlRoster = [
 "Mystery-Shows.json",
 "choice-classic-radio.json",
 "acast-com.json",
-	"Murder_By_Experts.json"
+	"Murder_By_Experts.json",
+	"78rpm_shira-berk.json",
+"wwIIarchive-audio.json"
+
 
 	 // === PY_MATRIX_END ===
 ];
